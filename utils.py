6a3198f06650aacfbd29638563755933c50ee3d5
Alexander Kuck <ask5rq@virginia.edu>
2014-04-01 14:40:35 -0400

    progress in understanding no correlation between python and js sha256

--- utils.py
+++ utils.py
@@ -13,6 +13,11 @@ def _hash(*args):
     """returns a long of the encoded params
     which are either strings or int (converted to hex)"""
     def upd(h, val):
+        print "+++++++++++++++++++++++++++++++++"
+        print "h      |", h.hexdigest()
+        print "upd-ele|", val
+        print "ele-typ|", type(val)
+
         typ = type(val)
         assert typ == int or typ == str or typ == long
         if typ == int:
@@ -29,19 +34,28 @@ def group_constants():
     """Returns the defined group constants for our implementation"""
     # N must have all capital letters
     N = """EEAF0AB9ADB38DD69C33F80AFA8FC5E86072618775FF3C0B9EA2314C9C256576D674DF7496EA81D3383B4813D692C6E0E0D5D8E250B98BE48E495C1D6089DAD15DC7D7B46154D6B6CE8EF4AD69B15D4982559B297BCF1885C529F566660E57EC68EDBC3C05726CC02FD4CBF4976EAA9AFD5138FE8376435B9FC61D2FC0EB06E3"""
+
     g = 2
     N = int(N, 16)
     k = _hash(str(N), g)
-    print "N | ", N
-    print "g | ", g
-    print "k | ", k
-    print '=========================='
+
+    print "----------------------------------------------------------"
+    print "N type | ", type(N)
+    print "N      | ", N
+    print "----------------------------------------------------------"
+    print "g type | ", type(g)
+    print "g      | ", g
+    print "----------------------------------------------------------"
+    print "k type | ", type(k)
+    print "k      | ", k
+    print "----------------------------------------------------------"
+    print "hash of nothing: ", hashlib.sha256("").hexdigest()
     return (N, g, k)
 
 
 if __name__ == '__main__':
-    print _hash('doge')
-    assert _hash('doge') == 89062479946239498583283658615240302315455651348145828341129845111945473878501
+    # print _hash('doge')
+    # assert _hash('doge') == 89062479946239498583283658615240302315455651348145828341129845111945473878501
     N, g, k = group_constants()
-    assert k == 80910912930494333589932475365044532708556454027074488358013717185805616192253 
+    # assert k == 80910912930494333589932475365044532708556454027074488358013717185805616192253 
 
